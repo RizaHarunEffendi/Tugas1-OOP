1185ea25d2c9b7f141bfc804325b426d9b3bddc9
Riza Harun Effendi <48326587+RizaHarunEffendi@users.noreply.github.com>
2019-03-07 23:37:22 +0700

    Create Tugas1.java

---
+++ Tugas1.java
@@ -0,0 +1,7 @@
+public class Halo {
+    public static void main(String[] args) {
+        System.out.println(" Nama    : Riza Harun Effendi ");
+        System.out.println(" NIM     : 17090126 ");
+        System.out.println(" Kelas   : 4B ");
+    }
+}
